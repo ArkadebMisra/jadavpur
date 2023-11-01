6cd25936342b39258c40edc651decce18d21716c
ArkadebMisra <misra.arkadeb@gmail.com>
2023-11-01 21:06:13 +0530

    first commit

---
+++ sem1/C/prog1.c
@@ -0,0 +1,42 @@
+// 1. displaying the first ten numbers in the Fibonacci series.
+
+// Problem 2 :​
+
+// If  a natural number is a square number, then it has to be​
+
+//       the sum of Successive Odd Numbers starting from 1. ​
+
+// Develop a flowchart to express the logic for displaying the first ten numbers in the series of such square nos.​
+
+// Represent the logic in the form of an algorithm which accepts the count of such numbers as input, and displays  each such square numbers along with its constituents.​
+
+// Finally write the C-code for the problem and execute it.
+
+
+// Problem 3:​
+
+// If the sum of cubes of each digit of a number is equal to the number itself, then the number is called an Armstrong number. ​
+
+//       For example, 153 = ( 13 ) + ( 53) + ( 33). ​
+
+// Develop a flowchart to express the logic for displaying all such numbers between 1 and 500.​
+
+// Represent the logic in the form of an algorithm which accepts the limit, and displays  each such number.​
+
+// Finally write the C-code for the problem and execute it.
+
+
+// [ARRAY: Problem 4]     Write a program in C to read n number of values in an array. After that, count a total number of duplicate elements in that array. Then copy the elements except the duplicate elements of that array into another array and display this array in reverse order.​
+
+// ​
+
+// [ARRAY: Problem 5]     Generate all prime numbers below 100. Count them.​
+
+// ​
+
+// [ARRAY: Problem 6]      Display the factors of all composite  numbers below 20 in a tabular form.
+
+
+
+#include <stdio.h>
+
