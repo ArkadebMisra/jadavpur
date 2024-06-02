b8275b8c860e951b1aab8fbc355216ebc67cd6d4
arka1581947 <misra.arkadeb@gmail.com>
2024-06-02 21:57:35 +0530

    os dssa

---
+++ sem2/os/a1/q1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Enter the length of the rectangle"
+read l
+
+echo "Enter the bredth of the rectangle"
+read b
+
+echo "the area of the rectangle is(caculated using expr)"
+area=`expr $l \* $b`
+echo "area $area"
+
+echo "the perimeter of the rectangle is(caculated using expr)"
+perimeter=`expr 2 \* \( $l + $b \)`
+echo "peremeter $perimeter"
+
+# echo "the area of the rectangle is(caculated using bc)"
+# area=$(echo "$l * $b" | bc)
+# echo "area $area"
